e41c25e8e2c7c33c1d20c4fa4005f7f5ac088712
Daniel <danielseniv@outlook.com>
2023-07-04 19:56:24 -0400

    Change element selector names to be more specific and identifiable, change style to match mockup

--- src/components/DateCalc.jsx
+++ src/components/DateCalc.jsx
@@ -1,6 +1,6 @@
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
-import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import enterArrow from "../../public/icon-arrow.svg"
 import "./DateCalc.scss"
 
 export const DateCalc = () => {
@@ -13,17 +13,17 @@ export const DateCalc = () => {
     },
     validationSchema: Yup.object({
       date: Yup.number()
-      .min(1, "Must be a number 1-31")
-      .max(31, "Must be number 1-31")
-      .required("This field is required"),
+        .min(1, "Must be a number 1-31")
+        .max(31, "Must be number 1-31")
+        .required("This field is required"),
       month: Yup.number()
-      .min(1, "Must be a number 1-12")
-      .max(12, "Must be a number 1-12")
-      .required("This field is required"),
+        .min(1, "Must be a number 1-12")
+        .max(12, "Must be a number 1-12")
+        .required("This field is required"),
       year: Yup.number()
-      .min(0, "Must be number 0-2023")
-      .max(2023, "Must be number 0-2023")
-      .required("This field is required"),
+        .min(0, "Must be number 0-2023")
+        .max(2023, "Must be number 0-2023")
+        .required("This field is required"),
     }),
     onSubmit: values => {
       alert(JSON.stringify(values, null, 2));
@@ -31,11 +31,11 @@ export const DateCalc = () => {
     },
   });
   return (
-    <div className='calcContainer'>
-        <form className="form" onSubmit={formik.handleSubmit}>
-          <div className="inputBoxes">
-          <div className="input">
-          <label htmlFor='date' className='label'>DAY</label>
+    <section className='calcContainer'>
+      <form className="form" onSubmit={formik.handleSubmit}>
+        <div className="headerContainer">
+          <div className="inputContainer">
+            <label className='label' htmlFor='date'>DAY</label>
             <input className='inputBox'
               id='date'
               name='date'
@@ -45,24 +45,24 @@ export const DateCalc = () => {
               value={formik.values.date}
             />
             {formik.touched.date && formik.errors.date ? (
-              <div className="fieldError">{formik.errors.date}</div> 
+              <div className="fieldError">{formik.errors.date}</div>
             ) : null}
           </div>
-          <div className="input">
+          <div className="inputContainer">
             <label className='label' htmlFor='month'>MONTH</label>
-            <input className="inputBox" 
+            <input className="inputBox"
               id='month'
               name='month'
               type='text'
               placeholder='MM'
               onChange={formik.handleChange}
-              value={formik.values.month}      
+              value={formik.values.month}
             />
             {formik.touched.month && formik.errors.month ? (
-              <div className="fieldError">{formik.errors.month}</div> 
+              <div className="fieldError">{formik.errors.month}</div>
             ) : null}
           </div>
-          <div className="input">
+          <div className="inputContainer">
             <label className='label' htmlFor='year'>YEAR</label>
             <input className="inputBox"
               id='year'
@@ -73,16 +73,18 @@ export const DateCalc = () => {
               value={formik.values.year}
             />
             {formik.touched.year && formik.errors.year ? (
-              <div className="fieldError">{formik.errors.year}</div> 
+              <div className="fieldError">{formik.errors.year}</div>
             ) : null}
           </div>
-          </div>
-        </form>
-        <div className='dividerContainer'>
-            <hr className='horizontalLine'></hr>
-            <ExpandCircleDownIcon sx={{width:"90px",height:"90px", color:"hsl(259, 100%, 65%)"}} type="submit" onClick={formik.handleSubmit}/>
         </div>
-    </div> 
+      </form>
+      <div className='submitContainer'>
+        <hr className='horizontalLine'></hr>
+        <button type="submit" onClick={formik.handleSubmit} className="submitArrow" >
+          <img src={enterArrow} alt="submitArrow" />
+        </button>
+      </div>
+    </section>
   )
 }
 
